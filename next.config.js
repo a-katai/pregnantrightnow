4c683ac53c243996dba7c025d5dc19167a57a3bc
Katai <info@katai.io>
2026-01-21 01:34:04 -0500

    Fix redirect pattern to handle root path

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: '/',
+        destination: 'https://chariahs.world/rsvp',
+        permanent: true, // 308 redirect
+      },
+      {
         source: '/:path*',
         destination: 'https://chariahs.world/rsvp',
         permanent: true, // 308 redirect
